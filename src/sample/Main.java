a66db852eb6a5da9fc38f4801ab5237842edd5d1
master <butaevil79@gmail.com>
2020-10-08 20:32:49 +1000

    up to date3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Привет мир");
-        primaryStage.setScene(new Scene(root, 750, 500));
+        primaryStage.setScene(new Scene(root, 750, 501));
         primaryStage.show();
     }
 

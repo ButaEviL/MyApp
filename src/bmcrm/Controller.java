d9a9e618b48121bce2cac5bd2b85273207cb09c4
master <butaevil79@gmail.com>
2020-10-08 21:45:04 +1000

    up to date, add set img

---
+++ src/bmcrm/Controller.java
@@ -0,0 +1,36 @@
+package bmcrm;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView setButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
--- src/bmcrm/Main.java
+++ src/bmcrm/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package bmcrm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Приветs мир");
-        primaryStage.setScene(new Scene(root, 750, 501));
+        Parent root = FXMLLoader.load(getClass().getResource("avtorisation.fxml"));
+        primaryStage.setTitle("Окно авторизации");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 

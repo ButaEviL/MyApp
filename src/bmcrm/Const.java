104dc4094dc86add3c27cc3b585b4034ebc8e4e0
master <butaevil79@gmail.com>
2020-10-09 01:10:54 +1000

    up to date, add set img

--- src/bmcrm/Const.java
+++ src/bmcrm/Const.java
@@ -1,13 +1,15 @@
 package bmcrm;
 
 public class Const {
-    //константы для связи с конкретными таблицами
 
     public static final String USER_TABLE = "users";
 
-    public static final String USER_ID = "idusers";
-    public static final String USER_LOGIN = "login";
-    public static final String USER_PASS = "password";
-
+    public static final String USERS_ID = "idusers";
+    public static final String USERS_FIRSTNAME = "firstname";
+    public static final String USERS_LASTNAME = "lastname";
+    public static final String USERS_USERNAME = "username";
+    public static final String USERS_PASSWORD = "password";
+    public static final String USERS_LOCATION = "location";
+    public static final String USERS_GENDER = "gender";
 
 }
--- src/bmcrm/Controllers/AutorisationController.java
+++ src/bmcrm/Controllers/AutorisationController.java
@@ -1,12 +1,16 @@
 package bmcrm.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class AutorisationController {
 
@@ -17,36 +21,40 @@ public class AutorisationController {
     private URL location;
 
     @FXML
-    private ImageView setButton;
+    private Button loginSignUpButton;
 
     @FXML
-    private TextField loginField;
+    private TextField login_Field;
 
     @FXML
-    private PasswordField passwordField;
+    private PasswordField password_Field;
 
     @FXML
-    private Button enterButton;
+    private Button authSignInButton;
 
     @FXML
     void initialize() {
 
-        enterButton.setOnAction(actionEvent -> {
-            String loginText = loginField.getText().trim();
-            String loginPassword = passwordField.getText().trim();
+        loginSignUpButton.setOnAction(actionEvent -> {
+            loginSignUpButton.getScene().getWindow().hide();
 
-            if (!loginText.equals("") && !loginPassword.equals("")) {
-                loginUser (loginText, loginPassword);
-            } else
-                System.out.println("Error login empty");
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/bmcrm/view/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
 
         });
 
 
 
     }
-
-    private void loginUser(String loginText, String loginPassword) {
-
-            }
 }

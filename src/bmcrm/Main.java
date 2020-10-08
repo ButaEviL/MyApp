9278f859b26bcdcfecd49be5fa4aa0b396d658fd
Ivan Voronov <butaevil@mail.ru>
2020-10-08 22:02:30 +1000

    test chanjes

--- src/bmcrm/Main.java
+++ src/bmcrm/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("avtorisation.fxml"));
-        primaryStage.setTitle("Окно  авторизации");
+        primaryStage.setTitle("Окно авторизации программы");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
